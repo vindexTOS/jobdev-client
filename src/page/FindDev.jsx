cdd3a29f1788493a497cc7cc0c8c0d550b80500d
VindexTOS <giorgikutateladze1998@gmail.com>
2023-07-24 00:25:24 +0400

    main component styling change

--- src/page/FindDev.jsx
+++ src/page/FindDev.jsx
@@ -4,7 +4,7 @@ import Resume from '../components/Resume'
 const FindDev = () => {
   const { data, nextUser, resumeIndex, save } = UseMainContext()
   const style = {
-    mainDiv: `h-[100vh] flex flex-col justify-around`,
+    mainDiv: `h-[100vh]  flex flex-col justify-around`,
     btnWrapper: `w-[100%] flex  items-center justify-around`,
     savebtn: `text-white w-[9rem] h-[3rem] bg-gradient-to-br from-green-400 to-green-500 hover:bg-gradient-to-bl focus:ring-2  focus:outline-none focus:ring-green-200 font-medium rounded-lg text-sm text-center shadow-lg `,
     nextbtn: `text-white w-[9rem] h-[3rem]  bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-2 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm shadow-lg  text-center`,
--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router-dom'
 import Header from '../components/Header'
 const Main = () => {
   const style = {
-    mainDiv: `w-[80%] h-[100vh] justify-around  flex flex-col  m-auto bg-gray-100 p-5 shadow-2xl	 `,
+    mainDiv: `w-[80%] h-[100%] justify-around  flex flex-col  m-auto bg-gray-100 p-5 shadow-2xl	 `,
   }
 
   return (
