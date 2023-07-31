1e31eaea9b7140ca791982b87f329dd62ac96016
VindexTOS <giorgikutateladze1998@gmail.com>
2023-07-31 23:29:31 +0400

    nav bar responsivnes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,9 @@ import { UseMainContext } from '../../context'
 import userdefault from '../assets/photos/userdefault.jpg'
 import Filter from './Filter'
 import UserDropDown from './UserDropDown'
-import { BsSearchHeart } from 'react-icons/bs'
+import { BsSearchHeart, BsFillPersonFill } from 'react-icons/bs'
 import { MdFilterList, MdZoomOutMap, MdZoomInMap } from 'react-icons/md'
+import { AiOutlineLogin } from 'react-icons/ai'
 import { motion as m } from 'framer-motion'
 
 const Header = () => {
@@ -26,7 +27,7 @@ const Header = () => {
       zoomIn
         ? `translate-y-[-85px]  top-0  sticky  absolute transition-all duration-300`
         : 'rounded-[50px]  relative  '
-    }    headerColor h-[6rem] p-10 z-50 w-[100%]     transition-all duration-300  flex items-center justify-around  text-gray-100  shadow-md  `,
+    }    headerColor h-[6rem] p-10 z-50 w-[100%]  max_smm1:gap-10     transition-all duration-300  flex items-center justify-around  text-gray-100  shadow-md  `,
   }
 
   const showFitlerMenu = () => {
@@ -55,23 +56,24 @@ const Header = () => {
         onMouseEnter={showFitlerMenu}
         onMouseLeave={hideFilterMenu}
       >
-        <button className="flex  items-center font-mono justify-around w-[9rem] text-[1.4rem]">
-          <MdFilterList className="text-[2rem]" />
+        <button className="flex   max_smm1:text-[12px]  max_smm1:w-auto  max_smm1:justify-center  max_smm1:gap-1  items-center font-mono justify-around w-[9rem] text-[1.4rem]">
+          <MdFilterList className="text-[2rem]  max_smm1:text-[1.5rem] " />
           Filter
         </button>
       </div>
       <button
         onClick={() => navigation('/')}
-        className="text-4xl font-mono	 font-mono flex items-center justify-around"
+        className="text-4xl font-mono  max_smm1:text-[1.5rem] 	 font-mono flex items-center justify-around"
       >
         FindDev <BsSearchHeart />
       </button>
       {!StateAuth.userData.sub ? (
-        <button
+        <div
+          className="flex  hover:bg-[#fe3c72]  max_smm1:text-[12px]  items-center justify-center gap-2 outline outline-[1px] outline-white rounded-[20px] p-2 cursor-pointer  "
           onClick={() => DispatchAuth({ type: 'AUTH_POP_UP', payload: true })}
         >
-          Register
-        </button>
+          <AiOutlineLogin /> <h1>Login</h1>
+        </div>
       ) : (
         <div
           className="bg-yellow-400 p-[2px] rounded-[50%] cursor-pointer relative"
